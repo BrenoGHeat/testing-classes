45880c8de738b2732bc686d6e2de116102a8648a
josedasilva123 <55949988+josedasilva123@users.noreply.github.com>
2023-12-06 10:03:40 -0300

    prisma unit test with mocks

--- src/__tests__/units/createTodo.test.ts
+++ src/__tests__/units/createTodo.test.ts
@@ -1,19 +1,14 @@
-import { prisma } from "../../database/prisma"
 import { TodoServices } from "../../services/todo.services";
-import { todoCreateBodyMock } from "../__mocks__/todo.mocks";
+import { prismaMock } from "../__mocks__/prisma";
+import { todoCreateBodyMock, todoMock } from "../__mocks__/todo.mocks";
 
 describe("Unit test: create todo", () => {
-    beforeEach(async () => {
-        await prisma.todo.deleteMany();
-    })
+   test("create todo should work correctly", async () => {
+      const todoServices = new TodoServices();
 
-    test("create todo should work correctly", async () => {
-        const todoServices = new TodoServices();
+      prismaMock.todo.create.mockResolvedValue(todoMock);
+      const data = await todoServices.create(todoCreateBodyMock);
 
-        const data = await todoServices.create(todoCreateBodyMock);
-
-        expect(data.id).toBeDefined();
-        expect(data.title).toBe(todoCreateBodyMock.title);
-        expect(data.content).toBe(todoCreateBodyMock.content);
-    })
-})
+      expect(data).toStrictEqual(todoMock);
+   });
+});
