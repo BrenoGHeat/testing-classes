73474c6d85f4927f6b537e30ac01460d8582e964
josedasilva123 <55949988+josedasilva123@users.noreply.github.com>
2023-12-08 10:57:01 -0300

    unit test basic

--- jest.config.ts
+++ jest.config.ts
@@ -4,5 +4,4 @@ export default {
     preset: 'ts-jest',
     testEnvironment: 'node',
     testMatch: ["**/__tests__/units/**/*.[jt]s?(x)"],
-    setupFilesAfterEnv: ["./src/__tests__/__mocks__/prisma.ts"]
   };
